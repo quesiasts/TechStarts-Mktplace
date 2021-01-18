5468580210bba87b081d7c5e2e4838c02b611ebb
Derli Machado <dam.machado1@gmail.com>
2021-01-18 13:47:15 -0300

    Refactor: Moldes.

--- backend/models/produto.py
+++ backend/models/produto.py
@@ -1,6 +1,38 @@
 class Product:
-  def __init__(self, name: str, description: str, price: float , id: int = None) -> None:
-    self.id = id
-    self.name = name
-    self.description = description
-    self.price = price
+    def __init__(self, name: str, description: str, price: float , id: int = None) -> None:
+        self.__id = id
+        self.__name = name
+        self.__description = description
+        self.__price = price
+  
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def description(self):
+        return self.__description
+        
+    @property
+    def price(self):
+        return self.__price
+        
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @description.setter
+    def description(self, description):
+        self.__description = description
+
+    @price.setter
+    def price(self, price):
+        self.__price = price
