c07e61239c410906b1d47ecf40480e9fd6e669ff
Matheus Abreu <matheus.abreu@olist.com>
2021-01-19 18:35:34 -0300

    Refactored Log Model to SQLAlchemy

--- backend/models/log.py
+++ backend/models/log.py
@@ -1,47 +1,18 @@
-class Log:
-  def __init__(self, description: str, date: str, hour: str, action: str, id: str = None) -> None:
-    self.id = id
-    self.date = date
-    self.hour = hour    
-    self.description = description
-    self.action = action
-  
-  @property
-  def id(self):
-    return self.__id
+from sqlalchemy import Column, String, Date, func, Time
 
-  @property
-  def description(self):
-    return self.__description
-  
-  @property
-  def date(self):
-    return self.__date
-  
-  @property
-  def hour(self):
-    return self.__hour
-  
-  @property
-  def action(self):
-    return self.__action
+from backend.models.base_model import BaseModel
 
-  @id.setter
-  def id(self, id):
-      self.__id = id
-  
-  @description.setter
-  def description(self, description):
-    self.__description = description
 
-  @date.setter
-  def date(self, date):
-    self.__date = date
-  
-  @hour.setter
-  def hour(self, hour):
-    self.__hour = hour
+class Log(BaseModel):
+    __tablename__ = 'log'
+    description = Column(String(length=200), nullable=False)
+    action = Column(String(length=200), nullable=False)
+    date = Column(Date, server_default=func.current_date(), nullable=False)
+    time = Column(Time, server_default=func.current_time(), nullable=False)
 
-  @action.setter
-  def action(self, action):
-    self.__action = action
+    def __init__(self, description: str, action: str, date: str = None, hour: str = None, id: str = None) -> None:
+        self.id = id
+        self.date = date
+        self.hour = hour
+        self.description = description
+        self.action = action
