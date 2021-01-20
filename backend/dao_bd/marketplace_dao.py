e8923ab4603a8a2913405c28ee5235c6da062ff0
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-20 10:02:22 -0300

    MarketplaceDao adjusted to inherit from BaseDao

--- backend/dao_bd/marketplace_dao.py
+++ backend/dao_bd/marketplace_dao.py
@@ -1,44 +1,7 @@
-from .connection import Connection
-
-from .base_dao import BaseDao
+from sqlalchemy.orm import sessionmaker
+from backend.dao_bd.base_dao import BaseDao
 from backend.models.marketplace import Marketplace
 
 class MarketplaceDao(BaseDao):
-    def create(self, marketplace: Marketplace)-> list:
-        query = f""" INSERT INTO marketplaces
-                            (NAME, DESCRIPTION)
-                            VALUES
-                            ('{marketplace.name}',
-                            '{marketplace.description}'); """
-        super().execute(query)
-            
-
-    def read_by_id(self, id: int) -> Marketplace:        
-        query = f"SELECT name, description, id FROM marketplaces WHERE ID={id}"
-        result = super().read(query) [0]
-        marketplace = Marketplace(result[0], result[1], result[2])
-        return marketplace
-
-
-    def read_all(self) -> list:
-        query = f"SELECT name, description, id FROM marketplaces"
-        result_list = super().read(query)
-        marketplaces = []              
-        for result in result_list:
-            marketplace = Marketplace(result[0], result[1], result[2])        
-            marketplaces.append(marketplace)        
-        return marketplaces
-
-
-    def update(self, marketplace: Marketplace) -> None:        
-        query = f"""UPDATE marketplaces
-                                SET 
-                                name = '{marketplace.name}',
-                                description = '{marketplace.description}'
-                                WHERE id = '{marketplace.id}'"""
-        super().execute(query)
-        
-
-    def delete(self, id: int) -> None:        
-        query = f"DELETE FROM marketplaces WHERE id = '{id}'"
-        super().execute(query)
+    def __init__(self):
+        super().__init__(Marketplace)
