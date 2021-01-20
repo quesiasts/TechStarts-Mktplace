363240b42db0af7050497f9f05e527d907dbc2a2
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-20 11:21:37 -0300

    Removed type_entity from init and added name for log creation

--- backend/controller/marketplace_controller.py
+++ backend/controller/marketplace_controller.py
@@ -3,9 +3,6 @@ from backend.dao_bd.marketplace_dao import MarketplaceDao
 
 
 class MarketplaceController(BaseController):
-    def __init__(self, type_entity):
+    def __init__(self):
         self.__dao = MarketplaceDao()
-        self.type_entity = type_entity
-        super().__init__(self.__dao, self.type_entity)
-
-
+        super().__init__(self.__dao, 'Marketplace')
