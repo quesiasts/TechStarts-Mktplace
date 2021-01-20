fcba2b2e4f05231b619482c72dfc29ae02bc060a
Matheus Abreu <matheus.abreu@olist.com>
2021-01-20 09:49:28 -0300

    Refactored BaseController

--- backend/controller/base_controller.py
+++ backend/controller/base_controller.py
@@ -1,34 +1,40 @@
-from .log_controller import LogController
+from sqlalchemy.orm.exc import NoResultFound
+
+from backend.controller.log_controller import LogController
+from backend.dao_bd.base_dao import BaseDao
+from backend.models.base_model import BaseModel
+
 log = LogController()
 
+
 class BaseController:
-  def __init__(self, dao, type_entity):
-    self.__dao = dao
-    self.type_entity = type_entity
+    def __init__(self, dao: BaseDao, domain_name: str):
+        self.__dao = dao
+        self.__domain_name = domain_name
 
-  def create(self, model: object) -> None:    
-    self.__dao.create(model)
-    log.create(f"{model.name} criado(a)", "cadastrando")
-      
+    def create(self, model: BaseModel) -> None:
+        self.__dao.save(model)
+        log.create(f"{self.__domain_name} criado(a)", "cadastrando")
 
-  def read_all(self) -> list:
-    result = self.__dao.read_all()
-    log.create(f"{self.type_entity} listados", "listando")
-    return result
-        
-  
-  def read_by_id(self, id:int) -> object:
-    result = self.__dao.read_by_id(id)
-    log.create(f"{self.type_entity} lido", "lendo")
-    return result   
-      
+    def read_all(self) -> list:
+        result = self.__dao.read_all()
+        log.create(f"{self.__domain_name} listados", "listando")
+        return result
 
-  def update(self, model: object) -> None:    
-    self.__dao.update(model)
-    log.create(f"{model.name} atualizado(a)", "atualizando")
-      
+    def read_by_id(self, id: int) -> BaseModel:
+        try:
+            result = self.__dao.read_by_id(id)
+        except NoResultFound:
+            raise Exception('Item not Found.')
+        log.create(f"{self.__domain_name} lido", "lendo")
+        return result
 
-  def delete(self, id:int):
-    self.__dao.delete(id)
-    log.create(f"{self.type_entity} deletado(a)", "deletando")
-   
+    def update(self, model: BaseModel) -> None:
+        self.read_by_id(model.id)
+        self.__dao.save(model)
+        log.create(f"{self.__domain_name} atualizado(a)", "atualizando")
+
+    def delete(self, id: int):
+        result = self.read_by_id(id)
+        self.__dao.delete(result)
+        log.create(f"{self.__domain_name} deletado(a)", "deletando")
