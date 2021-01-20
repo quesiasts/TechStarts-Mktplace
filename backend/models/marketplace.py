8ebf9556e3b1f1a2893993d12a9fe6f701138f7f
JotaCanutto <joaopedro.canutto@gmail.com>
2021-01-20 13:11:18 -0300

    imports adjusted to make code cleaner

--- backend/models/marketplace.py
+++ backend/models/marketplace.py
@@ -1,12 +1,12 @@
-import sqlalchemy as sql
+from sqlalchemy import String, Column
 from backend.models.base_model import BaseModel
 
 
 class Marketplace(BaseModel): 
     __tablename__ = 'marketplace'
 
-    name = sql.Column( sql.String(length=200), nullable=False )
-    description = sql.Column( sql.String(length=200), nullable=True )
+    name = Column( String(length=200), nullable=False )
+    description = Column( String(length=200), nullable=True )
 
     def __init__(self, name:str, description:str) -> None:
         self.name = name
