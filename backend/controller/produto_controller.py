f0b17e4bd9cef6b4a8b285356ac2e21d4c796bbb
lucasljoliveira <lucasljoliveira@hotmail.com>
2021-01-20 10:24:51 -0300

    Alterado o ProductController para passar o parametro domain_name

--- backend/controller/produto_controller.py
+++ backend/controller/produto_controller.py
@@ -5,8 +5,8 @@ from .base_controller import BaseController
 from backend.dao_bd.produto_dao import ProductDao
 
 class ProductController(BaseController):
-    def __init__(self, type_entity):
+    def __init__(self):
         self.__dao = ProductDao()
-        self.type_entity = type_entity
-        super().__init__(self.__dao, self.type_entity)
+        domain_name = "Produto"
+        super().__init__(self.__dao, domain_name)
 
