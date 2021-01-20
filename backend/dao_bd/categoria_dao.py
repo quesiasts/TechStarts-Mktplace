d6b7107dc6329366897e208a73b7a771297dc0a6
Derli Machado <dam.machado1@gmail.com>
2021-01-20 12:54:39 -0300

    recaforing model, controller and dao in category

--- backend/dao_bd/categoria_dao.py
+++ backend/dao_bd/categoria_dao.py
@@ -1,46 +1,13 @@
-from .connection import Connection
+from sqlalchemy.orm import sessionmaker
 
-from .base_dao import BaseDao
+from backend.dao_bd.base_dao import BaseDao
 from backend.models.categoria import Category
 
 class CategoryDao(BaseDao):
-    def create(self, categoria: Category)-> list:
-        query = f""" INSERT INTO CATEGORY
-                            (NAME, DESCRIPTION)
-                            VALUES
-                            ('{categoria.name}',
-                            '{categoria.description}'); """
-        super().execute(query)
-            
-
-    def read_by_id(self, id: int) -> Category:        
-        query = f"SELECT name, description, id FROM category WHERE ID={id}"
-        result = super().read(query) [0]
-        categoria = Category(result[0], result[1], result[2])
-        return categoria
-
-
-    def read_all(self) -> list:
-        query = f"SELECT name, description, id FROM category"
-        result_list = super().read(query)
-        categorias = []              
-        for result in result_list:
-            categoria = Category(result[0], result[1], result[2])        
-            categorias.append(categoria)        
-        return categorias
-
-
-    def update(self, categoria: Category) -> None:        
-        query = f"""UPDATE category
-                                SET 
-                                name = '{categoria.name}',
-                                description = '{categoria.description}'
-                                WHERE id = '{categoria.id}'"""
-        super().execute(query)
-        
-
-    def delete(self, id: int) -> None:        
-        query = f"DELETE FROM category WHERE id = '{id}'"
-        super().execute(query)
+    def __init__(self):
+        super().__init__(Category) 
 
     
+
+    
+   
