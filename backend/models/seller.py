331927f8f1402c075e911afcb21211aca66e1946
Matheus Abreu <matheus.abreu@olist.com>
2021-01-20 15:05:53 -0300

    Formatted file to PEP8

--- backend/models/seller.py
+++ backend/models/seller.py
@@ -1,14 +1,15 @@
 from sqlalchemy import Column, String
+
 from backend.models.base_model import BaseModel
+
 
 class Seller(BaseModel):
     __tablename__ = 'seller'
     name = Column(String(length=200))
     phone = Column(String(length=200))
     email = Column(String(length=200))
-        
 
-    def __init__(self, name:str, phone:str, email:str, id:int=None):
+    def __init__(self, name: str, phone: str, email: str, id: int = None):
         self.name = name
         self.phone = phone
         self.email = email
