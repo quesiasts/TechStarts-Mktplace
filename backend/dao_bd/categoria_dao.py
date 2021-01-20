2a17c5c889a80e2a078b4f831128927476a0e7f4
Derli Machado <dam.machado1@gmail.com>
2021-01-20 13:24:14 -0300

    Fix: typehint in categoria_dao.

--- backend/dao_bd/categoria_dao.py
+++ backend/dao_bd/categoria_dao.py
@@ -4,10 +4,10 @@ from backend.dao_bd.base_dao import BaseDao
 from backend.models.categoria import Category
 
 class CategoryDao(BaseDao):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__(Category) 
 
     
 
-    
+
    
